ad067b23bf364c6daa77b1e15e2131b7134e1ff4
samirzjadhav <samirzjadhav@gmail.com>
2025-12-09 20:33:07 +0530

    feat: added new things on layout and page

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "AI Travel Planner",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className="max-w-5xl mx-auto p-6">{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center p-6">
       <h1 className="text-4xl font-bold !mb-4">AI Travel Planner</h1>
       <p className="mb-6 max-w-xl text-center">
-        Generate a day-by-day itinerary using Ai. Try a simple or create your
-        custom plan.
+        Give a destination, days and budget — the AI will generate a day-by-day
+        itinerary, images, map pins, and cost estimates.
       </p>
       <div className="flex gap-4">
         <Link
@@ -15,6 +15,9 @@ export default function Home() {
           className="!px-6 py-3 !bg-sky-600 text-white rounded-lg shadow hover:opacity-90 "
         >
           Generate Plan
+        </Link>
+        <Link href="/saved" className="px-6 py-3 border rounded-md ">
+          Saved Plan
         </Link>
       </div>
     </main>
