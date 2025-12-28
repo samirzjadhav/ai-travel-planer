915f46f27e953d6304b0755fb0eb11d3546b1bf4
samirzjadhav <samirzjadhav@gmail.com>
2025-12-28 18:25:25 +0000

    feat: added new navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
 
-const Navbar = () => {
+export default function Navbar(){
   return (
-    <div>
-       <h1>Navbar</h1>
-    </div>
+    <nav className="bg-white shadow-sm">
+      <div className="max-w-5xl mx-auto p-4 flex items-center justify-between">
+        <Link href="/" className="font-bold">AI Travel Planner</Link>
+        <div className="flex gap-3">
+          <Link href="/generate" className="text-sm">Generate</Link>
+          <Link href="/saved" className="text-sm">Saved</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
-
-export default Navbar
